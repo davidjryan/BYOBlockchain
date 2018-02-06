71a3d42cac48d3cbe59b26ae52e1c57cef7df203
David Ryan <djrdev@icloud.com>
2018-02-06 14:45:42 -0700

    add schema to transaction table

---
+++ db/migrations/20180206143733_add-ids-transactinos.js
@@ -0,0 +1,21 @@
+exports.up = function (knex, Promise) {
+  return Promise.all([
+    knex.schema.table('transactions', function (table) {
+      table.integer('to').unsigned();
+      table.foreign('to').references('wallets.id');
+      table.integer('from').unsigned();
+      table.foreign('from').references('wallets.id');
+    })
+  ]);
+};
+
+exports.down = function (knex, Promise) {
+  return Promise.all([
+    knex.schema.table('transactions', function (table) {
+      table.integer('to').unsigned();
+      table.foreign('to').references('wallets.id');
+      table.integer('from').unsigned();
+      table.foreign('from').references('wallets.id');
+    })
+  ]);
+};
