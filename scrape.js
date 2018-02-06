b45b10da00a5663d368c6ff17ffbf10e797ae34e
David Ryan <djrdev@icloud.com>
2018-02-05 19:48:16 -0700

    etherscan.io addresses scrape

---
+++ scrape.js
@@ -0,0 +1,33 @@
+const Nightmare = require('nightmare');
+const nightmare = Nightmare({ show: true });
+const fs = require('fs');
+
+nightmare
+  .goto('https://etherscan.io/accounts')
+  .evaluate(() => {
+    const allAddresses = document.querySelectorAll('td a');
+
+    const addressInfo = [];
+
+    for (let i = 0; i < allAddresses.length; i++) {
+      let address = allAddresses[i].innerText;
+
+      addressInfo.push({ address })
+    }
+
+    return addressInfo
+  })
+  .end()
+  .then(result => {
+    const output = JSON.stringify(result, null, 2);
+    fs.writeFile('./wallet-data.json', output, 'utf8', (error) => {
+      if (error) {
+        return console.log(error)
+      }
+    })
+    console.log('File saved')
+    console.log(result)
+  })
+  .catch(error => {
+    console.error(error);
+  });
