3cbf8a1e794ba9a33adfa28cab9c857aa2e9ac68
David Ryan <djrdev@icloud.com>
2018-02-07 16:30:07 -0700

    test and production knex edits

--- knexfile.js
+++ knexfile.js
@@ -12,4 +12,23 @@ module.exports = {
     },
     useNullAsDefault: true
   },
+  test: {
+    client: 'pg',
+    connection: 'postgres://localhost/mockchain_test',
+    useNullAsDefault: true,
+    migrations: {
+      directory: __dirname + '/db/migrations'
+    },
+    seed: {
+      directory: __dirname + '/db/seeds/test'
+    }
+  },
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL + `?ssl=true`,
+    migrations: {
+      directory: __dirname + '/db/migrations'
+    },
+    useNullAsDefault: true
+  }
 };
