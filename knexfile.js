09a8a959dde7fc875bcbe87d20b67928d6cc8a40
Jesse Packwood <Jesse@Jesse.local>
2018-02-07 17:13:37 -0700

    fix test environment

--- knexfile.js
+++ knexfile.js
@@ -1,15 +1,33 @@
-// Update with your config settings.
 module.exports = {
 
-  development: {
-    client: 'pg',
-    connection: 'postgres://localhost/mockchain',
-    migrations: {
-      directory: __dirname + '/db/migrations'
-    },
-    seeds: {
-      directory: __dirname + '/db/seeds/dev'
-    },
-    useNullAsDefault: true
-  },
+ development: {
+   client: 'pg',
+   connection: 'postgres://localhost/mockchain',
+   migrations: {
+     directory: __dirname + '/db/migrations'
+   },
+   seeds: {
+     directory: __dirname + '/db/seeds/dev'
+   },
+   useNullAsDefault: true
+ },
+ test: {
+   client: 'pg',
+   connection: 'postgres://localhost/mockchain_test',
+   useNullAsDefault: true,
+   migrations: {
+     directory: __dirname + '/db/migrations'
+   },
+   seeds: {
+     directory: __dirname + '/db/seeds/test'
+   }
+ },
+ production: {
+   client: 'pg',
+   connection: process.env.DATABASE_URL + `?ssl=true`,
+   migrations: {
+     directory: __dirname + '/db/migrations'
+   },
+   useNullAsDefault: true
+ }
 };
